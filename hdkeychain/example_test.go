0a566ec627cde6e59b8007b9a62ab027d54ae097
9z25 <rsenecaljr@gmail.com>
2019-12-19 14:04:05 -0800

    Update example_test.go

--- hdkeychain/example_test.go
+++ hdkeychain/example_test.go
@@ -7,8 +7,8 @@ package hdkeychain_test
 import (
 	"fmt"
 
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcutil/hdkeychain"
+	"github.com/9z25/btcd/chaincfg"
+	"github.com/9z25/btcutil/hdkeychain"
 )
 
 // This example demonstrates how to generate a cryptographically random seed
