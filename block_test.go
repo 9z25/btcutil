41d6baa7541c779e4bbfb3ca4add5254cbcf6f7c
9z25 <rsenecaljr@gmail.com>
2019-12-19 14:11:58 -0800

    Update block_test.go

--- block_test.go
+++ block_test.go
@@ -11,9 +11,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 	"github.com/davecgh/go-spew/spew"
 )
 
