5599d22597b44bc9acc02374407c79d842f7fd42
9z25 <rsenecaljr@gmail.com>
2019-12-19 14:19:29 -0800

    Update coins.go

--- coinset/coins.go
+++ coinset/coins.go
@@ -9,9 +9,9 @@ import (
 	"errors"
 	"sort"
 
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil"
 )
 
 // Coin represents a spendable transaction outpoint
