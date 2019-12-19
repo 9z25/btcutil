5a9314728ec675d85b710110356c92aa86ff9147
9z25 <rsenecaljr@gmail.com>
2019-12-19 14:22:26 -0800

    Update txsort_test.go

--- txsort/txsort_test.go
+++ txsort/txsort_test.go
@@ -11,8 +11,8 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil/txsort"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil/txsort"
 )
 
 // TestSort ensures the transaction sorting works according to the BIP.
