554c17d00d6477eb1b7c913377b640e30f6f69d1
9z25 <rsenecaljr@gmail.com>
2019-12-19 14:17:40 -0800

    Update example_test.go

--- bloom/example_test.go
+++ bloom/example_test.go
@@ -9,9 +9,9 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil/bloom"
+	"github.com/9z25/btcd/chaincfg/chainhash"
+	"github.com/9z25/btcd/wire"
+	"github.com/9z25/btcutil/bloom"
 )
 
 // This example demonstrates how to create a new bloom filter, add a transaction
