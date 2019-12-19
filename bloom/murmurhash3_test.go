85b0816c6e239f0a12b2caa477bc0e208ecb4c1a
9z25 <rsenecaljr@gmail.com>
2019-12-19 14:19:08 -0800

    Update murmurhash3_test.go

--- bloom/murmurhash3_test.go
+++ bloom/murmurhash3_test.go
@@ -7,7 +7,7 @@ package bloom_test
 import (
 	"testing"
 
-	"github.com/btcsuite/btcutil/bloom"
+	"github.com/9z25/btcutil/bloom"
 )
 
 // TestMurmurHash3 ensure the MurmurHash3 function produces the correct hash
